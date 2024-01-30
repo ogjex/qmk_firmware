f487f039382b5aa8f0d4963b31f07746e513ef66
ogjex <81006912+ogjex@users.noreply.github.com>
2024-01-30 10:29:52 +0100

    refactor tapdance feature

--- keyboards/ferris/keymaps/ogjex/features/tapdance.h
+++ keyboards/ferris/keymaps/ogjex/features/tapdance.h
@@ -1,4 +1,35 @@
 #pragma once
 
-#include QMK_KEYBOARD_H
+// tapdance.h
+#ifndef TAPDANCE_H
+#define TAPDANCE_H
 
+#include "action.h"
+#include "action_code.h"
+#include "keycodes.h"
+
+// Add any other necessary includes here
+
+// Declare functions or variables related to tap dance keys
+
+typedef struct {
+    bool is_press_action;
+    bool recording;
+    td_state_t state;
+} td_tap_t;
+
+typedef struct {
+  bool is_press_action;
+  int state;
+} tap;
+
+// create a global instance of the tapdance state type
+static td_tap_t tap_state = {.state = TD_NONE};
+
+// define functions
+td_state_t cur_dance(tap_dance_state_t *state);
+void alt_finished (tap_dance_state_t *state, void *user_data);
+void alt_reset (tap_dance_state_t *state, void *user_data);
+
+
+#endif // TAPDANCE_H
---
+++ keyboards/ferris/keymaps/ogjex/keymap.h
@@ -0,0 +1,15 @@
+// keymap.h
+#ifndef KEYMAP_H
+#define KEYMAP_H
+
+#include QMK_KEYBOARD_H
+
+// Add any other necessary includes here
+
+// Declare functions or variables related to the keymap
+#define _QWERTY 0
+#define _SYMB 1
+#define _NMPAD 2
+#define _NAV 3
+
+#endif // KEYMAP_H
