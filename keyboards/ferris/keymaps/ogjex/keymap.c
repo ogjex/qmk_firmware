4666fac4d9d52f1922c52fa75c60f4ad3cecb6ac
ogjex <81006912+ogjex@users.noreply.github.com>
2024-02-06 11:21:34 +0100

    add dot for numpad and equals to e in numpad layer

--- keyboards/ferris/keymaps/ogjex/keymap.c
+++ keyboards/ferris/keymaps/ogjex/keymap.c
@@ -30,11 +30,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_NMPAD] = LAYOUT(
     // Numpad layer, from layer 1--------------                                     ---------------------------------------------
-    KC_TRNS, KC_NO, KC_NO, KC_PPLS, KC_PMNS,                                        LSFT(KC_0), KC_7, KC_8, KC_9, TD(TD_BSPACE),
+    KC_TRNS, KC_NO, , KC_PPLS, KC_PMNS,                                        LSFT(KC_0), KC_7, KC_8, KC_9, TD(TD_BSPACE),
     // ----------------------------------------                                     ---------------------------------------------
     TD(TD_APP_TAB), KC_NO, KC_NO, KC_PSLS, KC_PAST,                                        KC_PERC, KC_4, KC_5, KC_6, KC_ENT,
     // ----------------------------------------                                     ---------------------------------------------
-    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS,                                    KC_0, KC_1, KC_2, KC_3, KC_PEQL,
+    KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_TRNS,                                    KC_0, KC_1, KC_2, KC_3, KC_DOT,
     // ----------------------------------------                                     ---------------------------------------------
                                 KC_TRNS, KC_TRNS,                                   TO(_FKEY), KC_TRNS
     ),
