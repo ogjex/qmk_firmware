d25ca26d4a9c0b5d1117891947a54017b5fb8924
ogjex <81006912+ogjex@users.noreply.github.com>
2024-02-20 08:59:53 +0100

    add double hold with shift + alt for better windows shortcuts

--- keyboards/ferris/keymaps/ogjex/features/tapdance.c
+++ keyboards/ferris/keymaps/ogjex/features/tapdance.c
@@ -308,6 +308,9 @@ void td_osm_sft_ctl_alt(tap_dance_state_t *state, void *user_data) {
             set_oneshot_mods(MOD_BIT(KC_LCTL) | MOD_BIT(KC_LSFT));
             break;
         case TD_DOUBLE_HOLD:
+            set_oneshot_mods(MOD_BIT(KC_LSFT) | MOD_BIT(KC_LALT));
+            break;
+        case TD_TRIPLE_HOLD:
             set_oneshot_mods(MOD_BIT(KC_LCTL) | MOD_BIT(KC_LSFT) | MOD_BIT(KC_LALT));
             break;
         default:
