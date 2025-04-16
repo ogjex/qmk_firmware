ed1e589eff0dcd62d3dab79d6b855a51f5266eb1
ogjex <m.agnu.ha@gmail.com>
2025-04-16 16:12:42 +0200

    fix home row mods + missing percentage and backslash

--- keyboards/ferris/keymaps/ogjex/keymap.c
+++ keyboards/ferris/keymaps/ogjex/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base key input layer--------------------                                     -----------------------------------------------
     KC_Q, KC_W, KC_E, KC_R, KC_T,                                                   KC_Y, KC_U, KC_I, TD(TD_OE_DK), KC_P,
     //-----------------------------------------                                     -----------------------------------------------
-    TD(TD_AA_DK), KC_S, KC_D, KC_F, KC_G,                                           KC_H, KC_J, KC_K, KC_L, TD(TD_AE_ENTER),
+    TD(TD_AA_DK), MT(MOD_LSFT,KC_S), MT(MOD_LSFT,KC_D), MT(MOD_LGUI,KC_F), MT(MOD_LALT,KC_G),   MT(MOD_LALT,KC_H), MT(MOD_LGUI,KC_J), MT(MOD_LCTL,KC_K), MT(MOD_LCTL,KC_L), TD(TD_AE_ENTER),
     //-----------------------------------------                                     -----------------------------------------------
     KC_Z, KC_X , KC_C, KC_V, TD(TD_DELETE),                                         KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
     //-----------------------------------------                                     -----------------------------------------------
@@ -19,11 +19,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYMB] = LAYOUT(
     // Signs and symbols layer, from layer 0---                                     ----------------------------------------------
-    TD(TD_ESC_TM), ALGR(KC_2), LSFT(KC_3), ALGR(KC_4), LSFT(KC_4),                  LSFT(KC_6), LSFT(KC_7), KC_NO, KC_SLSH, LSFT(KC_SLSH),
+    TD(TD_ESC_TM), ALGR(KC_2), LSFT(KC_3), ALGR(KC_4), LSFT(KC_5),                  LSFT(KC_6), LSFT(KC_7), KC_NO, KC_SLSH, LSFT(KC_SLSH),
     // ----------------------------------------                                     ----------------------------------------------
     KC_EQL, LSFT(KC_EQL), LSFT(KC_2), KC_NUHS, KC_PAST,                             LSFT(KC_1), LSFT(KC_8), LSFT(KC_9), LSFT(KC_MINS), ALGR(KC_EQL),
     // ----------------------------------------                                     ----------------------------------------------
-    KC_NUBS, LSFT(KC_NUBS), ALGR(KC_RBRC), KC_RBRC, KC_GRV,                         ALGR(KC_8), ALGR(KC_7), ALGR(KC_0), ALGR(KC_9), TO(_NAV),
+    KC_NUBS, LSFT(KC_NUBS), ALGR(KC_RBRC), KC_RBRC, ALGR(KC_NUBS),                  ALGR(KC_8), ALGR(KC_7), ALGR(KC_0), ALGR(KC_9), TO(_NAV),
     // ----------------------------------------                                     ----------------------------------------------
                             KC_TRNS, KC_TRNS,                                       TO(_NMPAD), KC_TRNS
     ),
